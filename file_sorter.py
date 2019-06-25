33f0583cfa667fbe6914750709d96aa47a2baefc
mooremok <mooremok@163.com>
2019-06-26 00:31:49 +0800

    a big upgrade from Aaron

--- file_sorter.py
+++ file_sorter.py
@@ -1,21 +1,11 @@
 import os
 import shutil
 
-#自动获取工程文件当前位置
-current_path = os.path.dirname(__file__)
+"""
+Set constants
+"""
 
-#遍历目录的内容
-def get_all_files(current_path):
-    files = os.listdir(current_path)
-    list_dir = []
-    for file in files:
-        list_dir.append(file)
-    return list_dir
-
-#传入当前路径，实例化
-files_list = get_all_files(current_path)
-
-#预设文件夹，即各类型文件归档的存放地
+# Set the presuposed folders for storing files object.
 images = '照片'
 folders = '文件夹'
 documents = '文档'
@@ -24,55 +14,114 @@ others = 'others'
 frontends = '前端文件'
 backends = '后端文件'
 
-#启动脚本时检查预设文件夹是否存在，无则创建
+# listing all the presuposed folders.
 folders_list = [images, folders, documents, zips, others, backends, frontends]
 
-for list in folders_list:
-    if list not in files_list:
-        os.makedirs(list)
+# sort the file_attr, use to judge the file which folder should be sorted into.
+images_type = ['.jpg', '.png', '.bmg', '.tif', '.gif']
+documents_type = ['.doc', '.docx', '.wps', '.ppt', '.pptx', '.xls', '.xlsx', '.txt', '.md']
+zips_type = ['.zip', '.rar', '.7z']
+frontends_type = ['.html', '.htm', '.css']
+backends_type = ['.php', '.java', '.py']
+others_type = ['.bat']
 
-#判断文件格式以便归档
-images_list = ['.jpg', '.png', '.bmg', '.tif', '.gif']
-documents_list = ['.doc', '.docx', '.wps', '.ppt', '.xls', '.xlsx', '.txt', '.md']
-zips_list = ['.zip', '.rar']
-others_list = ['.bat']
-frontends_list = ['.html', '.htm', '.css']
-backends_list = ['.php', 'java', '.py']
 
+"""Direction of improvement:
+
+Round 1: moving around
+    1. More meaningful variable naming (e.g. images_type rathan than images_list)
+    2. More structural
+    3. Change comments from Chinese to English.
+
+Round 2: 
+    1. Tidy up. 
+    2. Rewrite the structure of folder list (images...others, images_type...others_type). 
+        Maybe use tuple instead of list.
+    3. Rewrite function bodies to be more structural.
+    4. Use of exception."""
+
+def get_cwd():
+    """Return the current working directory."""    
+    curr_file = os.path.abspath(__file__)
+    curr_folder = os.path.dirname(curr_file)
+    return curr_folder
+
+def get_all_files(current_path):
+    """Return a list containing files of current_path."""    
+    # TODO: check if I need list_dir stuffs.
+    files = os.listdir(current_path)
+    list_dir = []
+    for file in files:
+        list_dir.append(file)
+    return list_dir
+
+def create_folder(files, folders):
+    """Create if folder does not exist."""
+    # Checking in the current_path, create the presuposed folder that not matching.    
+    for item in folders:
+        if item not in files: # Not matching in the current_path, then create one.
+            os.makedirs(item)
+
+def place_files(cwd, files):
+    """Sort and archive files in given path."""
+    # except for folder
+    for file in files:    
+        split_text = os.path.splitext(file)
+        attr = attrs(split_text[1])
+        
+        if attr == "1":
+            new_path = os.path.join(cwd, images, file)
+            shutil.move(file, new_path)
+        elif attr == "2":
+            new_path = os.path.join(cwd, documents, file)
+            shutil.move(file, new_path)
+        elif attr == "3":
+            new_path = os.path.join(cwd, zips, file)
+            shutil.move(file, new_path)
+        elif attr == "4":
+            new_path = os.path.join(cwd, others, file)
+            shutil.move(file, new_path)
+        elif attr == "5":
+            new_path = os.path.join(cwd, frontends, file)
+            shutil.move(file, new_path)
+        elif attr == "6":
+            new_path = os.path.join(cwd, backends, file)
+            shutil.move(file, new_path)
+    print("File placement is completed.")
+
+def place_folder(cwd, files, folders_list):
+    """Place folder"""    
+    for item in files:
+        if os.path.isdir(item) and item not in folders_list:
+            new_path = os.path.join(cwd, folders, item)
+            shutil.move(item, new_path)
+    print("Folder placement is completed.")
 
 def attrs(splitext):
     splitext = splitext.lower()
-    if splitext in images_list:
+    if splitext in images_type:
         return "1"
-    elif splitext in documents_list:
+    elif splitext in documents_type:
         return "2"
-    elif splitext in zips_list:
+    elif splitext in zips_type:
         return "3"
-    elif splitext in others_list:
+    elif splitext in others_type:
         return "4"
-    else:
+    elif splitext in frontends_type:
         return "5"
+    elif splitext in backends_type:
+        return "6"
+    else:
+        return "7"
 
-#文件归档（除文件夹）
-for file in files_list:    
-    split_text = os.path.splitext(file)
-    attr = attrs(split_text[1])
-    
-    if attr == "1":
-        new_path = os.path.join(current_path, images, file)
-        shutil.move(file, new_path)
-    elif attr == "2":
-        new_path = os.path.join(current_path, documents, file)
-        shutil.move(file, new_path)
-    elif attr == "3":
-        new_path = os.path.join(current_path, zips, file)
-        shutil.move(file, new_path)
-    elif attr == "4":
-        new_path = os.path.join(current_path, others, file)
-        shutil.move(file, new_path)
 
-#文件夹归档    
-for file in files_list:
-    if os.path.isdir(file) and file not in folders_list:
-        new_path = os.path.join(current_path, folders, file)
-        shutil.move(file, new_path)
+# get the current_path 
+cwd = get_cwd()
+# go through all the files object
+all_files = get_all_files(cwd)
+# check the presuposed folders 
+create_folder(all_files, folders_list)
+# sort and archive the files except for folder
+place_files(cwd, all_files)
+# sort and archive the folders
+place_folder(cwd, all_files, folders_list)
